ea871d106ac5749057716e2ea8a0e6f5e95b3e96
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2021-10-13 23:36:58 -0300

    Add fields for uploading data files

--- src/app/modules/import-data/import-data.module.ts
+++ src/app/modules/import-data/import-data.module.ts
@@ -4,14 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ImportDataRoutingModule } from './import-data-routing.module';
 import { ImportFilesComponent } from './import-files';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    ImportFilesComponent
-  ],
+  declarations: [ImportFilesComponent],
   imports: [
     CommonModule,
-    ImportDataRoutingModule
-  ]
+    ImportDataRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+  ],
 })
-export class ImportDataModule { }
+export class ImportDataModule {}
