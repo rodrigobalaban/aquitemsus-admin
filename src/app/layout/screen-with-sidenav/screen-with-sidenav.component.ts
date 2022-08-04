160c84bfed6a8399260a95191c88ccdd37df2b6a
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2022-08-03 23:24:53 -0300

    Add role check on side menu

--- src/app/layout/screen-with-sidenav/screen-with-sidenav.component.ts
+++ src/app/layout/screen-with-sidenav/screen-with-sidenav.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/interfaces';
+import { AuthService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-screen-with-sidenav',
   templateUrl: './screen-with-sidenav.component.html',
   styleUrls: ['./screen-with-sidenav.component.scss']
 })
-export class ScreenWithSidenavComponent implements OnInit {
+export class ScreenWithSidenavComponent {
+  user: User;
 
-  constructor() { }
+  constructor(private _authService: AuthService) { 
+    this.user = this._authService.getUserAuthSession();
+  }
 
-  ngOnInit(): void {
+  get userIsAdmin(): boolean {
+    return this.user.role === "Administrator";
   }
 
 }
